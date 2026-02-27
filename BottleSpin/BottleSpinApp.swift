5f9ae24b336cd758b3350f7145b68f0cd8838ba1
Crasoner <279700125@qq.com>
2026-02-27 15:36:35 +0800

    Initial Commit

---
+++ BottleSpin/BottleSpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleSpinApp.swift
+//  BottleSpin
+//
+//  Created by 王世一 on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct BottleSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
